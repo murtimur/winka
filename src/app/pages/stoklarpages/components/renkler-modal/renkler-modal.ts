eba9784f06742df109bc514e267c0f6b9bed8e4b
Murat <murat@Murat-MacBook-Pro.local>
2026-06-08 15:14:14 +0300

    renkler modal

---
+++ src/app/pages/stoklarpages/components/renkler-modal/renkler-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renkler-modal',
+  imports: [],
+  templateUrl: './renkler-modal.html',
+  styleUrl: './renkler-modal.scss',
+})
+export class RenklerModal {}
